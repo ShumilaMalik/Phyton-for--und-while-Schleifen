9e637b4fb1666f7b0d4e9ee11249a855fb85c9c3
ShumilaMalik <102217768+ShumilaMalik@users.noreply.github.com>
2023-03-21 08:42:59 +0100

    Benutzereingabe mit Break

--- while-schleife.py
+++ while-schleife.py
@@ -1,11 +1,13 @@
 # Syntax der while-Schleife
 # while bedingung:
 
-count = 1
+"""count = 1
 # die Schleife wird ausgeführt, solange count kleiner oder gleich 5 ist
 while count <= 5:
     print(count)
     count += 1 #Erhöht count um 1 nach jeder Iteration
+
+#____________________________________________________________________________________________________
 
 i = 1
 n = 5
@@ -13,3 +15,17 @@ n = 5
 while i <= n:
     print(i)
     i = i + 1
+"""
+#___________________________________________________________
+
+# Die Schleife wird ausgeführt, solange der Benutzer nicht "ende" eingibt.
+
+while True:
+    user_input = input("Geben Sie etwas ein (oder 'ende', um das Programm zu beenden): ")
+
+    if user_input == 'ende':
+        break # Beendet die Schleife, wenn der Benutzer "ende" eingibt
+
+    print(f"Sie haben {user_input} eingeben.")
+
+#_____________________________________________________________
